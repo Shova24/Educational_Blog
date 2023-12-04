cef42c77353f79a96b0538f8e35280c3963653a7
Shova24 <jannatul35-2568@diu.edu.bd>
2023-12-04 20:53:12 +0600

    feat: navbar component structure created

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import CourseList from "@/components/Homepage/CourseList";
 import Hero from "@/components/Homepage/Hero";
 import Navbar from "@/components/style/Navbar";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
--- src/components/style/Navbar.jsx
+++ src/components/style/Navbar.jsx
@@ -1,42 +1,90 @@
 "use client";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
-import Link from "next/link";
 
-function Navbar() {
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Languages, Search, Sun, SunMoon } from "lucide-react";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Input } from "../ui/input";
+
+const Navbar = () => {
+  const [isDark, setIsDark] = useState(false);
+  const changeMode = () => {
+    console.log(isDark);
+    setIsDark(!isDark);
+  };
   return (
-    <div className="flex flex-wrap items-center max-w-screen-xl p-4 mx-auto justify-left">
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
-      <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-amber-500 ">
-        <Link href="/">CSS</Link>
-      </span>
+    <div className="flex justify-between mx-2 my-4">
+      <div className="flex gap-4">
+        <div>Logo</div>
+        <div className="flex gap-4">
+          <div>DSA</div>
+          <div>Tutorials</div>
+          <div>Database</div>
+          <div>Web Development</div>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center w-full max-w-sm space-x-2">
+          <Input type="email" placeholder="Email" />
+          <Search />
+        </div>
+        <div>
+          {isDark ? (
+            <SunMoon className="w-6 h-6 text-purple-400" onClick={changeMode} />
+          ) : (
+            <Sun className="w-6 h-6 text-purple-400" onClick={changeMode} />
+          )}
+        </div>
+        <Select>
+          <SelectTrigger className="w-full">
+            <div className="flex items-center justify-center gap-2">
+              <Languages className="w-4 h-4 text-purple-400" />
+              <SelectValue />
+            </div>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="bn">Bangla</SelectItem>
+              <SelectItem value="en">English</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button variant="custom">Sign In</Button>
+      </div>
     </div>
+    // <div className="flex flex-wrap items-center max-w-screen-xl p-4 mx-auto justify-left">
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    //   <span className="px-3 py-1 m-2 border-2 rounded-lg hover:border-purple-500 ">
+    //     <Link href="/">CSS</Link>
+    //   </span>
+    // </div>
   );
-}
+};
 
 export default Navbar;
